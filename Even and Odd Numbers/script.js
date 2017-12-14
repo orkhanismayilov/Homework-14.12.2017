eeeb4db4044a216304d0ea7ce7e232338528849e
Orkhan Ismayilov <orikismayilov@outlook.com>
2017-12-15 00:33:37 +0400

    Homework #3.03 v1.00

---
+++ Even and Odd Numbers/script.js
@@ -0,0 +1,16 @@
+var x = Number(prompt("Enter first number"));
+var a = Math.round(x);
+
+var y = Number(prompt("Enter second number"));
+var b = Math.round(y);
+
+var z = Number(prompt("Enter third number"));
+var c = Math.round(z);
+
+console.log(a, b, c);
+
+if (a%2==0||b%2==0||c%2==0){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
