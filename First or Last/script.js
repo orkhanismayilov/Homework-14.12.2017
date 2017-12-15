8b51a7c1caf719b13b0d10cd3f2d31026ee9394e
Orkhan Ismayilov <34453008+orkhanismayilov@users.noreply.github.com>
2017-12-15 18:12:44 +0400

    Homework #3.01 v1.01

--- First or Last/script.js
+++ First or Last/script.js
@@ -1,23 +1,18 @@
 var x = Number(prompt("Insert 3 digit number"));
 var a = 0;
-var b = 0;
 var c = 0;
 
 if (x>99&&x<1000){
         var a = parseInt(x/100);
-        // console.log(a);
-        var b = parseInt((x-a*100)/10);
-        // console.log(b);
-        var c = parseInt((x-a*100)-b*10);
-        // console.log(c);
-        if (a==b&&b==c){
+        console.log(a);
+        var b = x%10;
+        console.log(b);
+        if (a==b){
             console.log("=");
-        }else if (a>=b&&a>=c){
+        }else if (a>b){
             console.log(a);
-        }else if (b>=a&&b>=c){
+        }else{
             console.log(b);
-        }else if (c>=a&&c>=b){
-            console.log(c);
         }
 }else{
     console.log("Invalid number!")
