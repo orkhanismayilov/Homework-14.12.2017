5974b71005e7e0aa6a5c46c1a39dcb955c4ebe01
Orkhan Ismayilov <34453008+orkhanismayilov@users.noreply.github.com>
2017-12-15 15:28:27 +0400

    Homework #3.05 v1.01

--- Wardrobe/script.js
+++ Wardrobe/script.js
@@ -5,8 +5,12 @@ var c = Number(prompt("Please, enter the depth of the wardrobe"));
 var x = Number(prompt("Please, enter the width of the door"));
 var y = Number(prompt("Please, enter the height of the door"));
 
-if (a*b<x*y||b*c<x*y||a*c<x*y){
-    console.log("Yes");
+if (a>0&&b>0&&c>0&&x>0&&y>0){
+    if (a*b<x*y||b*c<x*y||a*c<x*y){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
 }else{
-    console.log("No");
+    console.log("Wrong dimensions!")
 }
